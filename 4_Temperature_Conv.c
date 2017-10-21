22086bf2b90abfc303ef05195cf6c0d56fe0becf
Maria Besa Joy M. Ortuyo <mmortuyo@carsu.edu.ph>
2017-10-21 13:08:13 +0800

    initial code

---
+++ 4_Temperature_Conv.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+  char choice;
+  float celsius, fahrenheit;
+  printf("This is a temperature conversion program.\n");
+  printf("Press [c] to convert Celsius to Fahrenheit.\n");
+  printf("      [f] to convert Fahrenheit to Celsius.\n");
+  printf("Choice: ");
+  scanf("%c", &choice);
+  if(choice=='c' || choice=='C'){
+    printf("\nInput Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit=celsius*1.8 + 32;
+    printf("\nFahrenheit is %.2f.\n", fahrenheit);
+  }else if(choice=='f' || choice=='F'){
+    printf("\nInput Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    celsius=(fahrenheit-32)*5/9;
+    printf("\Celsius is %.2f.\n", celsius);
+  }else{
+    printf("\nError: Invalid input.");
+  }
+  return 0;
+}
